8f6e103fdb16fa7b7fba60ac050785dfd8c62b90
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2026-06-22 12:22:40 +0530

    Fix cv not downloading problem

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -1,2 +1,2 @@
-export const RESUME_FILE_PATH = '/Docs/Kaveesha_Sewmini_CV.pdf';
-export const RESUME_FILE_NAME = 'Kaveesha_Sewmini_CV.pdf';
+export const RESUME_FILE_NAME = 'KaveeshaSewmini_CV_SoftwareEngineer.pdf';
+export const RESUME_FILE_PATH = `${import.meta.env.BASE_URL}Docs/${RESUME_FILE_NAME}`;
